c4bb66e85ab6db585f4d04aadadbcd6fb3b2c6f2
rasyeeed <muhammadnurrasyid@gmail.com>
2023-03-12 22:20:07 +0700

    menambahkan implementasi dengan table-driven

--- tpmodul4_1302213039/Program.cs
+++ tpmodul4_1302213039/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mod4_1302213039
+{
+    internal class program
+    {
+        public enum dataKota
+        {
+            Batununggal,
+            Kujangsari,
+            Mengger,
+            Wates,
+            Cijaura,
+            Jatisari,
+            Margasari,
+            Sekejati,
+            Kebonwaru,
+            Maleer,
+            Samoja
+        };
+        public class getKodePos
+        {
+            public static int kode(dataKota kota)
+            {
+                int[] kodepos = { 40266, 40287, 40267, 40256, 40287, 40286, 40286, 40286, 40272, 40274, 40273 };
+                return kodepos[(int)kota];
+            }
+        }
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("----------------------------");
+            dataKota dkota = dataKota.Batununggal;
+            int kode = getKodePos.kode(dkota);
+            Console.WriteLine("Kelurahan: " + dkota + "\nkodepos: " + kode);
+        }
+    }
+}
